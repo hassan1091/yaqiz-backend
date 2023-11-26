f0dfccdf9e159bf7afe1c0bee87a176e6337df05
حسن الخليف <roh.baba.11@gmail.com>
2023-11-26 09:47:26 +0300

    get the data from xlsm files

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const xlsx = require('xlsx')
+
+const app = express();
+
+const redData = xlsx.utils.sheet_to_json(xlsx.readFile(__dirname +"\\" +"red.xlsm").Sheets["Sheet1"])
+const yellwData = xlsx.utils.sheet_to_json(xlsx.readFile(__dirname +"\\"+"yellow.xlsm").Sheets["Sheet1"])
+const greenData = xlsx.utils.sheet_to_json(xlsx.readFile(__dirname +"\\"+"green.xlsm").Sheets["Sheet1"])
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/red', (req, res)=> {
+    res.send(redData);
+})
+app.get('/yellow', (req, res)=> {
+    res.send(yellwData);
+})
+app.get('/green', (req, res)=> {
+    res.send(greenData);
+})
+
+
 
 app.listen(process.env.PORT || 8080, function() {
   console.log("Server started on port 8080");
