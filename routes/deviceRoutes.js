a24025df120e2f7627104465b5fe391f0d7c1a05
حسن الخليف <roh.baba.11@gmail.com>
2023-12-03 20:02:09 +0300

    add get all device and if id has or not

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -11,6 +11,15 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/all/:id", (req, res) => {
+  let sql =
+    "SELECT Device_ID, (SELECT EXISTS (SELECT 1 FROM user_has_device WHERE user_has_device.Device_Device_ID = device.Device_ID AND user_has_device.User_Employee_ID = 4)) AS has FROM device;";
+  db.query(sql, req.params.id, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
 router.get("/:id", (req, res) => {
   let sql =
     "SELECT device.Device_ID, device.Device_Location, vital_signs.Priority FROM `user_has_device` JOIN `user` ON user.Employee_ID = user_has_device.User_Employee_ID JOIN `device` ON device.Device_ID = user_has_device.Device_Device_ID JOIN device_has_vital_signs ON device.Device_ID = device_has_vital_signs.Device_Device_ID JOIN vital_signs ON device_has_vital_signs.Vital_Signs_Vital_ID = vital_signs.Vital_ID WHERE User_Employee_ID = ?;";
